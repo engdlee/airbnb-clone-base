dac861311852becd54d42f110f1e287daae5e5c8
David Lee <ing.jdplee@gmail.com>
2023-04-14 16:37:59 -0400

    Properties screen (Loading your listings with server component)

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,8 +1,37 @@
 import prisma from '@/app/libs/prismadb';
 
-export default async function getListings() {
+export interface IListingsParams {
+    userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
+}
+
+export default async function getListings(params: IListingsParams) {
     try {
+        const {
+            userId,
+            roomCount,
+            guestCount,
+            bathroomCount,
+            locationValue,
+            startDate,
+            endDate,
+            category,
+        } = params;
+
+        let query: any = {};
+
+        if (userId) {
+            query.userId = userId;
+        }
+
         const listings = await prisma.listing.findMany({
+            where: query,
             orderBy: {
                 createdAt: 'desc',
             },
@@ -12,6 +41,7 @@ export default async function getListings() {
             ...listing,
             createdAt: listing.createdAt.toISOString(),
         }));
+
         return safeListings;
     } catch (error: any) {
         throw new Error(error);
